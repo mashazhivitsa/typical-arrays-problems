a50da68e138faf561e589db2889c71f8785f8bc7
mashazhivitsa <maho.miname@gmail.com>
2020-10-03 22:41:00 +0300

    feat: updated functions in index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+    array.sort((a, b) => a - b);
+    return array.shift();
+    } else { 
+    return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+    array.sort((a, b) => b - a);
+    return array.shift();
+    } else { 
+    return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length !== 0) {
+      var sum = 0;
+      for (var i = 0; i < array.length; i++) {
+      sum += array[i];
+    } 
+    return sum / array.length;
+  } else {
+    return 0;
+  }      
 }
